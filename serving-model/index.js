71cbbf2934270079ba283f8203c7af155c1db959
DCLab-GitHub <kimkyeongjun273@gmail.com>
2021-07-20 12:19:53 +0900

    ADD upload, delete function in index.js, ADD npm package express-fileupload

--- serving-model/index.js
+++ serving-model/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs')
+const fileUpload = require('express-fileupload')
 const model_path = `${__dirname}/models/`
+
+app.use(fileUpload())
 app.get('/', (req, res) => {
   res.send(`
   <html>
@@ -11,13 +14,23 @@ app.get('/', (req, res) => {
   <head>
   <body>
     <h3> list of models </h3>
-    <p> /list/model <p>
+    <p> curl http://(server-ip):3000/list/model </p>
     <br>
+
     <h3> download specific model using wget</h3>
-    <p> /get/model/(model name)/(model version) <p>
+    <p> wget http://(server-ip):3000/get/model/(model name)/(model version) </p>
     <br>
+
     <h3> download specific latest model using wget</h3>
-    <p> /get/model/(model name) <p>
+    <p> wget http://(server-ip):3000/get/model/(model name) </p>
+    <br>
+
+    <h3> upload model file using curl </h3>
+    <p> curl -F 'modelFile=@/path/to/file/(model name)' http://(server-ip):3000/upload/model </p>
+    <br>
+    
+    <h3> delete specific model file using curl </h3>
+    <p> curl -X DELETE http://(server-ip):3000/delete/(model name) </p>
   </body>
   </html>`)
 })
@@ -65,17 +78,40 @@ app.get('/get/model/:modelname', (req, res)=>{
 })
 
 app.get('/get/model', (req, res) => {
-  
-  console.log(model_path)
   res.send('download page!')
 })
 
 app.get('/list/model', (req, res) => {
   fs.readdir(model_path, (err, models)=>{
     if (err) {
-      console.log(err)
+      return res.status(400).send(err)
     }
     res.send(models)
+  })
+})
+
+app.post('/upload/model', (req, res)=>{
+  if (!req.files || Object.keys(req.files).length === 0){
+    return res.status(400).send('No files were uploaded.')
+  }
+
+  modelFile = req.files.modelFile;
+  modelFile.mv(model_path + modelFile.name, (err)=>{
+    if (err){
+      return res.status(500).send(err)
+    }
+
+    res.send('File uploaded!')
+  })
+})
+
+app.delete('/delete/model/:filename', (req, res)=>{
+  console.log(req.params.filename)
+  fs.unlink(model_path + req.params.filename, (err)=>{
+    if (err) {
+      return res.status(500).send(err)
+    }
+    res.send('File deleted!')
   })
 })
 
